b0ab879bc729ec81486a6b92216e43e765db860e
Katie Lonsdale <katie952@gmail.com>
2023-02-15 09:11:01 -0700

    test: add name and age tests

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 RSpec.describe Student do
+  before(:each) do
+    @student = Student.new({name: "Morgan", age: 21})
+  end
   describe '#initialize' do
     it 'exists' do
-      student = Student.new({name: "Morgan", age: 21})
+      expect(@student).to be_instance_of(Student)
+    end
 
-      expect(student).to be_instance_of(Student)
+    it 'has a name' do
+      expect(@student.name).to eq('Morgan')
+    end
+
+    it 'has an age' do
+      expect(@student.name).to eq 21
     end
   end
 end
